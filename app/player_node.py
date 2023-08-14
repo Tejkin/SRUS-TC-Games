238e33705211472051e38b0e99f0383232cbcd0f
Tejkin <116251992+Tejkin@users.noreply.github.com>
2023-08-14 11:20:08 +0800

    initialised PlayerNode class

--- app/player_node.py
+++ app/player_node.py
@@ -1,3 +1,9 @@
+from typing import Optional
+
 class PlayerNode:
     def __init__(self, player):
         self._player = player
+        self.prev_node = Optional[PlayerNode] = None
+        self.next_node = Optional[PlayerNode] = None
+        
+    

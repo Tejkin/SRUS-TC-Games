e28e501680a3c6188b8885dad4de97df4cb9377b
Tejkin <116251992+Tejkin@users.noreply.github.com>
2023-08-14 11:25:48 +0800

    created getters for PlayerNode

--- app/player_node.py
+++ app/player_node.py
@@ -1,9 +1,16 @@
 from typing import Optional
-
+from player import Player
 class PlayerNode:
-    def __init__(self, player):
+    def __init__(self, player: Player = None):
         self._player = player
-        self.prev_node = Optional[PlayerNode] = None
-        self.next_node = Optional[PlayerNode] = None
+        self._prev_node = Optional[PlayerNode] = None
+        self._next_node = Optional[PlayerNode] = None
         
+    def get_player_info(self):
+        return self._player
     
+    def get_prev_node(self):
+        return self._prev_node
+    
+    def get_next_node(self):
+        return self._next_node

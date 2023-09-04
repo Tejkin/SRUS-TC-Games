3a1396e0564a7f21f31a1f1ed7754f1dcf72eff3
Tejkin <116251992+Tejkin@users.noreply.github.com>
2023-09-04 11:33:54 +0800

    changed filledplayerlist to be indendent

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -6,10 +6,12 @@ from app.player_list import PlayerList
 class TestPlayerList(unittest.TestCase):
     def setUp(self):
         self.emptyPlayerList = PlayerList()
-        self.filledPlayerList = self.emptyPlayerList
+        self.filledPlayerList = PlayerList()
         self.filledPlayerList.insert_node("Test")
+        
     def test_is_empty_true(self):
         self.assertEqual(self.emptyPlayerList.is_empty(), True)
+        
     def test_is_empty_false(self):
         self.assertEqual(self.filledPlayerList.is_empty(), False)
     

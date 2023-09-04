149d2e81a08a482b46b799a1152ab679ccdd4222
Tejkin <116251992+Tejkin@users.noreply.github.com>
2023-09-04 10:32:23 +0800

    created is empty function in player list

--- app/player_list.py
+++ app/player_list.py
@@ -3,3 +3,8 @@ from typing import Optional, Sequence
 class PlayerList:
     def __init__(self, values: Sequence = None):
         self._start = None
+        
+    def is_empty(self):
+        if self._start == None:
+            return True
+        return False
